eda48ad2a45841f4e53b328b9f52535b4698f0ef
congmaru <109335090+congmaru@users.noreply.github.com>
2023-03-21 10:07:30 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 76 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.py
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.py
@@ -1,45 +1,43 @@
-import sys
+#공주님을 구해라
 from collections import deque
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
+directy = [-1, 1, 0, 0]
+directx = [0, 0, -1, 1]
 
 def bfs():
-    ret = 10001
-    q = deque([(0, 0)])
-    visit[0][0] = 0
+    ret = 21e8
+    q = deque()
+    q.append([0,0])
+    used[0][0] = 0
 
     while q:
-        x, y = q.popleft()
+        now = q.popleft()
+        y,x = now[0],now[1]
 
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            dx = directx[i]+x
+            dy = directy[i]+y
 
-            if nx == n - 1 and ny == m - 1:
-                return min(ret, visit[x][y] + 1)
+            if dy == N - 1 and dx == M - 1:
+                return min(used[y][x]+1, ret)
 
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            if dy<0 or dx<0 or dy>=N or dx>=M :
                 continue
 
-            if graph[nx][ny] == 0 and visit[nx][ny] == 0:
-                visit[nx][ny] = visit[x][y] + 1
-                q.append((nx, ny))
+            if arr[dy][dx] == 0 and used[dy][dx] == 0:
+                used[dy][dx] = used[y][x]+1
+                q.append([dy,dx])
 
-            elif graph[nx][ny] == 2 and visit[nx][ny] == 0:
-                visit[nx][ny] = visit[x][y] + 1
-                ret = min(ret, visit[nx][ny] + abs(n - 1 - nx) + abs(m - 1 - ny))
-
+            elif arr[dy][dx] == 2 and used[dy][dx] == 0:
+                used[dy][dx] = used[y][x] +1
+                ret = min(ret, used[dy][dx]+abs(N-1-dy)+abs(M-1-dx))
     return ret
 
 
-n, m, t = map(int, sys.stdin.readline().split())
-graph = []
-for i in range(n):
-    graph.append(list(map(int, sys.stdin.readline().split())))
-visit = [[0] * m for i in range(n)]
+N, M, T = map(int,input().split()) #세로, 가로, 시간제한
+arr = [list(map(int,input().split())) for _ in range(N)]
+used = [[0] * M for _ in range(N)]
 
 ans = bfs()
 
-print("Fail" if ans > t else ans)
+print('Fail' if ans > T else ans)
