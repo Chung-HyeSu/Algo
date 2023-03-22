04cd495724da4265353a9dc212b080f7bed8f0ec
congmaru <109335090+congmaru@users.noreply.github.com>
2023-03-22 22:40:34 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 160 ms, Memory: 116692 KB -BaekjoonHub

--- 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.py
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.py
@@ -1,8 +1,12 @@
-#공주님을 구해라
+#공주님을 다시 구해라
 from collections import deque
+N,M,T = map(int,input().split()) #세로, 가로, 제한시간
+arr = [list(map(int,input().split())) for _ in range(N)]
 
-directy = [-1, 1, 0, 0]
-directx = [0, 0, -1, 1]
+used = [[0]*M for _ in range(N)]
+
+directy = [-1,1,0,0]
+directx = [0,0,-1,1]
 
 def bfs():
     ret = 21e8
@@ -15,28 +19,21 @@ def bfs():
         y,x = now[0],now[1]
 
         for i in range(4):
-            dx = directx[i]+x
             dy = directy[i]+y
+            dx = directx[i]+x
 
-            if dy == N - 1 and dx == M - 1:
+            if dy == N-1 and dx == M-1 :
                 return min(used[y][x]+1, ret)
 
-            if dy<0 or dx<0 or dy>=N or dx>=M :
-                continue
-
-            if arr[dy][dx] == 0 and used[dy][dx] == 0:
-                used[dy][dx] = used[y][x]+1
+            if dy<0 or dx<0 or dy>=N or dx>=M : continue
+            if used[dy][dx] != 0: continue
+            if arr[dy][dx] == 0:
+                used[dy][dx] = used[y][x] + 1
                 q.append([dy,dx])
-
-            elif arr[dy][dx] == 2 and used[dy][dx] == 0:
-                used[dy][dx] = used[y][x] +1
-                ret = min(ret, used[dy][dx]+abs(N-1-dy)+abs(M-1-dx))
+            elif arr[dy][dx] == 2:
+                used[dy][dx] = used[y][x] + 1
+                ret = min(ret,used[dy][dx]+abs(N-1-dy)+abs(M-1-dx))
     return ret
-
-
-N, M, T = map(int,input().split()) #세로, 가로, 시간제한
-arr = [list(map(int,input().split())) for _ in range(N)]
-used = [[0] * M for _ in range(N)]
 
 ans = bfs()
 
