e26f24d02f5691e182e6e196536fae34871c035a
Chung Hye su <109335090+ChungHyesoo@users.noreply.github.com>
2024-07-30 16:50:53 +0900

    [Silver II] Title: A → B, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,18 +1,19 @@
-def calcul(A, B):
+A, B = map(int,input().split())
+
+def calcul(A,B):
     cnt = 0
-    while B > A:
-        if B % 2 == 0:
-            B //= 2
-        elif B % 10 == 1:
-            B //= 10
+    while B>A:
+        if B%2 == 0:
+            B = B//2
+        elif B%10 == 1:
+            B = B//10
         else:
             break
         cnt += 1
 
     if A == B:
-        return cnt + 1
-    else:
+        return cnt+1
+    else: 
         return -1
 
-A, B = map(int, input().split())
-print(calcul(A, B))
+print(calcul(A,B))
