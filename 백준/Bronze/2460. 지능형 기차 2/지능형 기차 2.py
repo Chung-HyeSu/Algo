18665e774008f8798a73f54b7abc7fe6b7f68122
Chung Hye su <109335090+ChungHyesoo@users.noreply.github.com>
2024-07-31 19:14:49 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.py
@@ -0,0 +1,27 @@
+present = 0
+station = 0
+Max_present = -21e8
+
+for _ in range(10):
+    down, up = map(int,input().split())
+    predown_present = present - down
+    preup_present = present + up
+
+    Max_present = max(present, Max_present)
+    if predown_present<=0 :
+        present = 0
+    else:
+        present -= down
+
+    Max_present = max(present, Max_present)
+
+
+    if preup_present >= 10000:
+        present = 10000
+    else:
+        present += up
+
+    Max_present = max(present, Max_present)
+
+
+print(Max_present)
