fba0c4c16c8d4cc29f999a6cbbc49f713ede8cc3
Chung Hye su <109335090+ChungHyesoo@users.noreply.github.com>
2024-09-22 10:15:29 +0900

    [Gold V] Title: 암호 만들기, Time: 124 ms, Memory: 112064 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -0,0 +1,21 @@
+#1759 암호
+l,c = map(int,input().split())
+letters = sorted(list(map(str,input().split())))
+moeum = ['a','e','i','o','u']
+temp = []
+
+def back(idx,cn_mo, cn_ja):
+    if len(temp) == l:
+        if cn_mo >=1 and cn_ja >=2:
+            print(''.join(temp))
+        return
+
+    for i in range(idx,c):
+        temp.append(letters[i])
+        if letters[i] in moeum:
+            back(i+1, cn_mo+1, cn_ja)
+        else:
+            back(i+1, cn_mo, cn_ja+1)
+        temp.pop()
+
+back(0,0,0)
