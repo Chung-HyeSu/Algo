f4ab97f4633934193a962b07c18438eab28b2232
Chung Hye su <109335090+ChungHyesoo@users.noreply.github.com>
2024-09-22 11:21:12 +0900

    [Silver II] Title: N과 M (9), Time: 136 ms, Memory: 113740 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,17 +1,19 @@
-N, M = map(int, input().split())
-nums = sorted(list(map(int, input().split())))
-visited = [False]*N
+
+import sys
+input = sys.stdin.readline
+n, m = map(int,input().split())
+nums = sorted(list(map(int,input().split())))
+
+visited = [False]*n
 temp = []
-#사전순으로 증가하는 순으로 수열 출력하기, 중복되는건 제외
 
 def back():
-    global M
-    if len(temp) == M:
+    if len(temp) == m:
         print(*temp)
         return
 
     temp_num = 0
-    for i in range(N):
+    for i in range(n):
         if not visited[i] and temp_num != nums[i]:
             visited[i] = True
             temp.append(nums[i])
@@ -19,7 +21,6 @@ def back():
             back()
             visited[i] = False
             temp.pop()
-
 
 
 back()
