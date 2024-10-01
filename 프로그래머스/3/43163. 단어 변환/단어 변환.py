a0af30912a8c26f000161a8dd4f939132f671845
Chung HyeSu <109335090+Chung-HyeSu@users.noreply.github.com>
2024-10-01 23:25:05 +0900

    [level 3] Title: 단어 변환, Time: 0.45 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,34 +1,35 @@
-from collections import deque
-
-def solve(begin, target, words):
-    q = deque()
-    q.append([begin,0])
-    
-    visited = [False]*len(words)
-
-    
-    while q:
-        now, cnt = q.popleft()
-        if now == target:
-            return cnt
-        
-        for idx, word in enumerate(words):
-            dif = 0
-            for i in range(len(now)):
-                if word[i] != now[i] and not visited[idx]:
-                    dif += 1
-            if dif == 1: #같은게 2개일 때만 바꿀 수 있음. 
-                q.append([word,cnt+1])
-                visited[idx] = True
-    return 0
-                
-                    
-
 def solution(begin, target, words):
+    global answer
+    visited = [False]*len(words)
+    answer = 21e8
     
+    def is_same(cur,word):
+        dif = 0
+        for i in range(len(word)):
+            if cur[i] != word[i]:
+                dif += 1
+        return dif
+        
+    
+    def dfs(cur, cnt):
+        global answer
+        if cur == target:
+            answer = min(answer, cnt)
+            return
+        
+        for i,word in enumerate(words):
+            if is_same(cur,word) == 1 and not visited[i]:
+                visited[i] = True
+                dfs(word, cnt+1)
+                visited[i] = False
+                
+                
     if target not in words:
         return 0
     else:
-        return solve(begin, target, words)
-    
-    
+        dfs(begin,0)
+        
+        if answer == 21e8:
+            return 0
+        else:
+            return answer
