77716ab81c9f79c7797abc32e7b937f586e3edbd
Chung HyeSu <109335090+Chung-HyeSu@users.noreply.github.com>
2024-10-01 16:22:21 +0900

    [level 1] Title: 최소직사각형, Time: 9.53 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,12 @@
+def solution(sizes):
+    garo_fin = -21e8
+    sero_fin = -21e8
+    
+    for wallet in sizes:
+        garo, sero = max(wallet), min(wallet)
+        garo_fin = max(garo_fin, garo)
+        sero_fin = max(sero_fin, sero)
+    
+    answer = garo_fin*sero_fin
+        
+    return answer
