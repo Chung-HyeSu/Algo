ce1f014f3d66efbb0188ceca234a7695c5a5b223
Chung HyeSu <109335090+Chung-HyeSu@users.noreply.github.com>
2024-10-06 11:26:39 +0900

    [level 2] Title: 모음 사전, Time: 2.15 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -2,8 +2,7 @@ def solution(word):
     answer = []
     mouem = ['A','E','I','O','U']
     
-    
-    def make_words(cur, length):
+    def make_words(cur,length):
         if length == 0:
             answer.append(cur)
             return 
@@ -11,8 +10,11 @@ def solution(word):
         for mo in mouem:
             make_words(cur+mo, length-1)
     
+    
     for length in range(1,6):
         make_words('',length)
     
     answer.sort()
+    
+    
     return answer.index(word)+1
