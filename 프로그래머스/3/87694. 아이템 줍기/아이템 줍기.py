c7441b0d0840fec60fcb1b17f4349726f34ad5a9
Chung HyeSu <109335090+Chung-HyeSu@users.noreply.github.com>
2024-10-06 11:44:36 +0900

    [level 3] Title: 아이템 줍기, Time: 2.64 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,35 +1,48 @@
-from collections import deque
-
 def solution(rectangle, characterX, characterY, itemX, itemY):
-    def make_board():
-        board = [[0] * 102 for _ in range(102)]
-        for x1, y1, x2, y2 in rectangle:
-            for i in range(2*x1, 2*x2+1):
-                for j in range(2*y1, 2*y2+1):
-                    board[i][j] = 1
-        for x1, y1, x2, y2 in rectangle:
-            for i in range(2*x1+1, 2*x2):
-                for j in range(2*y1+1, 2*y2):
-                    board[i][j] = 0
-        return board
-
-    def bfs(board, start_x, start_y, end_x, end_y):
-        queue = deque([(start_x, start_y, 0)])
-        visited = [[False] * 102 for _ in range(102)]
-        visited[start_x][start_y] = True
+    global way
+    answer = 0
+    way = 21e8
+    
+    maps = [[0]*102 for _ in range(102)]
+    visited = [[False]*102 for _ in range(102)]
+    
+    def marking(x1,y1,x2,y2):
+        for x in range(x1,x2+1):
+            for y in range(y1,y2+1):
+                if x == x1 or x == x2 or y == y1 or y == y2:
+                    if maps[y][x] != 2:
+                        maps[y][x] = 1
+                else:
+                    maps[y][x] = 2
+                    
+    
+    def explore(x,y,fx,fy,cnt):
+        global way
+        if x == fx and y == fy:
+            way = min(way, cnt)
+            return 
         
-        while queue:
-            x, y, dist = queue.popleft()
-            if x == end_x and y == end_y:
-                return dist // 2
-            
-            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < 102 and 0 <= ny < 102 and board[nx][ny] == 1 and not visited[nx][ny]:
-                    queue.append((nx, ny, dist + 1))
-                    visited[nx][ny] = True
+        visited[y][x] = True
         
-        return -1
-
-    board = make_board()
-    return bfs(board, 2*characterX, 2*characterY, 2*itemX, 2*itemY)
+        directy = [-1,1,0,0]
+        directx = [0,0,-1,1]
+        
+        for i in range(4):
+            dy = y + directy[i]
+            dx = x + directx[i]
+            if 0<=dy<102 and 0<=dx<102:
+                if maps[dy][dx] == 1 and visited[dy][dx] == False:
+                    visited[dy][dx] = True
+                    explore(dx,dy,fx,fy,cnt+1)
+                    visited[dy][dx] = False
+        
+    
+    
+    
+    for rec in rectangle:
+        marking(2*rec[0], 2*rec[1], 2*rec[2], 2*rec[3])
+        
+    explore(2*characterX, 2*characterY, 2*itemX, 2*itemY, 1)
+    
+    
+    return way//2
