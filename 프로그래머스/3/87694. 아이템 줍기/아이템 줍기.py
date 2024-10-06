0f51ec253acc3b8ce83b940224221dba4db5f58a
Chung HyeSu <109335090+Chung-HyeSu@users.noreply.github.com>
2024-10-06 11:48:28 +0900

    [level 3] Title: 아이템 줍기, Time: 2.61 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,48 +1,46 @@
 def solution(rectangle, characterX, characterY, itemX, itemY):
     global way
     answer = 0
+
+    # 가장 바깥 테두리만 사용
+    # 모든 좌표는 1이상 50이하 -> range는 1, 51
+
+    maps = [[0] * 102 for _ in range(102)]
+    visited = [[False] * 102 for _ in range(102)]
     way = 21e8
-    
-    maps = [[0]*102 for _ in range(102)]
-    visited = [[False]*102 for _ in range(102)]
-    
-    def marking(x1,y1,x2,y2):
-        for x in range(x1,x2+1):
-            for y in range(y1,y2+1):
+
+    def marking(x1, y1, x2, y2):
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
                 if x == x1 or x == x2 or y == y1 or y == y2:
                     if maps[y][x] != 2:
                         maps[y][x] = 1
                 else:
                     maps[y][x] = 2
-                    
-    
-    def explore(x,y,fx,fy,cnt):
+
+    def explore(x, y, fx, fy, cnt):
         global way
         if x == fx and y == fy:
             way = min(way, cnt)
-            return 
-        
+            return
+
         visited[y][x] = True
-        
-        directy = [-1,1,0,0]
-        directx = [0,0,-1,1]
-        
+
+        directy = [-1, 1, 0, 0]
+        directx = [0, 0, -1, 1]
+
         for i in range(4):
-            dy = y + directy[i]
-            dx = x + directx[i]
-            if 0<=dy<102 and 0<=dx<102:
+            dy = directy[i] + y
+            dx = directx[i] + x
+            if 0 <= dy < 102 and 0 <= dx < 102:
                 if maps[dy][dx] == 1 and visited[dy][dx] == False:
                     visited[dy][dx] = True
-                    explore(dx,dy,fx,fy,cnt+1)
+                    explore(dx, dy, fx, fy, cnt + 1)
                     visited[dy][dx] = False
-        
-    
-    
-    
+
     for rec in rectangle:
-        marking(2*rec[0], 2*rec[1], 2*rec[2], 2*rec[3])
-        
-    explore(2*characterX, 2*characterY, 2*itemX, 2*itemY, 1)
-    
-    
-    return way//2
+        marking(2 * rec[0], 2 * rec[1], 2 * rec[2], 2 * rec[3])
+
+    explore(2 * characterX, 2 * characterY, 2 * itemX, 2 * itemY, 1)
+
+    return way // 2
