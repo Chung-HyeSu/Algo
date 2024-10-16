4780385d1111ff02dd2a1c0503a71b38a42c6853
Chung HyeSu <109335090+Chung-HyeSu@users.noreply.github.com>
2024-10-16 17:32:45 +0900

    [level 2] Title: 가장 큰 수, Time: 410.43 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,11 +1,17 @@
 def solution(numbers):
+    answer = ''
+    
     lst = list(map(str,numbers))
     
     def compare(x):
-        return x*(4//len(x)+1)
+        #정렬하는 조건
+        #문자열을 가지고 하는것
+        # 3, 30, 340
+        return x * (4//len(x)+1)
         
-    a = sorted(lst, key = compare, reverse=True)
-    answer = ''.join(a)
+    sorted_numbers = sorted(lst, key=compare, reverse=True)
+    answer = ''.join(sorted_numbers)
+
     
     
     return answer if int(answer) != 0 else '0'
