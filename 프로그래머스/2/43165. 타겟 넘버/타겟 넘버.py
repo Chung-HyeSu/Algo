6562ca933e923948bac872b68cd07729fb22b940
Chung HyeSu <109335090+Chung-HyeSu@users.noreply.github.com>
2024-11-10 15:03:31 +0900

    [level 2] Title: 타겟 넘버, Time: 366.45 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,22 +1,16 @@
-#더하거나 빼서 타겟넘버
-#numbers: 숫자배열, target: 목표
-
-def dfs(numbers,target,depth,cur):
+def dfs(numbers, target, depth, cur):
     global answer
     if depth == len(numbers):
         if cur == target:
             answer += 1
-        return
-    
-    #더하는 경우
+        return 
+
     dfs(numbers, target, depth+1, cur+numbers[depth])
-    
-    #빼는 경우
+
     dfs(numbers, target, depth+1, cur-numbers[depth])
     
-
 def solution(numbers, target):
     global answer
     answer = 0
-    dfs(numbers, target, 0,0)
+    dfs(numbers, target, 0, 0)
     return answer
