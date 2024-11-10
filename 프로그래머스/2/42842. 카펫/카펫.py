27c43969eee43f13da095c3687cf6958a3dff43c
Chung HyeSu <109335090+Chung-HyeSu@users.noreply.github.com>
2024-11-10 15:21:50 +0900

    [level 2] Title: 카펫, Time: 691.99 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,17 +1,10 @@
 def solution(brown, yellow):
     answer = []
     
-    # brown + yellow = 2*g+2*s+4+gs
-    # yellow = g*s
-    # brown = 2*g+2*s+4
-    
     for s in range(brown):
         for g in range(s,brown):
             if yellow == g*s and brown == 2*g+2*s+4:
                 answer.append(g+2)
                 answer.append(s+2)
-    
-    # print(answer)
-
     
     return answer
