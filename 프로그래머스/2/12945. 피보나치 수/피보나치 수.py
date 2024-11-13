55f9c1c1df3cdaa50ba94871c129fb315a5add13
Chung HyeSu <109335090+Chung-HyeSu@users.noreply.github.com>
2024-11-13 11:23:45 +0900

    [level 2] Title: 피보나치 수, Time: 445.03 ms, Memory: 456 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,12 +1,8 @@
 def solution(n):
-    
-    fib = [0]*(n+1)
+    fib = [0] * (n+1)
     fib[1] = 1
     
     for i in range(2,n+1):
         fib[i] = fib[i-1] + fib[i-2]
-    
-    
-    
     
     return fib[-1]%1234567
